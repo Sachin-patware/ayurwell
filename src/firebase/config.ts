106f7499f65717da2d75f54d14f0a6681253fea9
Sachin Patware <sachinpatware10@gmail.com>
2025-11-15 11:34:07 +0000

    export const firebaseConfig = {   "projectId": "studio-4469902418-85356"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4469902418-85356",
-  "appId": "1:852246079879:web:75287ec434e0c73103cb22",
-  "apiKey": "AIzaSyBM7ktkPsOfJ2fo9Wh4RK5-QhxWFwqSa0U",
-  "authDomain": "studio-4469902418-85356.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "852246079879"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 };
