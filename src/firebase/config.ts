dab4d5bd604aa6d59986fc841000210456823cc9
Sachin Patware <sachinpatware10@gmail.com>
2025-11-15 10:09:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4469902418-85356",
+  "appId": "1:852246079879:web:75287ec434e0c73103cb22",
+  "apiKey": "AIzaSyBM7ktkPsOfJ2fo9Wh4RK5-QhxWFwqSa0U",
+  "authDomain": "studio-4469902418-85356.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "852246079879"
+};
